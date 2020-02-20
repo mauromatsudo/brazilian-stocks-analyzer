ea6abbeb87072be5e6e6f19f3a271bba7eb5c370
mauromatsudo <maurao.matsudo@gmail.com>
2020-02-20 10:09:59 -0300

    "Fixed"the problem with the debt indicators that were incorrect

--- BovespaAnalys/utilities.py
+++ BovespaAnalys/utilities.py
@@ -25,6 +25,10 @@ class Data:
                 value = valuations[index].text
             values.append(value)
         fundamental_indicators = dict(zip(keys, values))
+        # because of the issue if more h3 tags in this are, tehere are some incicadors, that mus be
+        # change manually, this is not the best practice, so it must be changed
+        fundamental_indicators['Passivos / Ativos'] = fundamental_indicators['Dívida Líquida / EBITDA']
+        fundamental_indicators['Dívida Líq/EBIT'] = fundamental_indicators['Dívida Líquida / Patrimônio']
         return fundamental_indicators
     @property
     def price_indicators(self):
@@ -34,13 +38,25 @@ class Data:
     def profit_indicators(self):
         indicators = ['ROE', 'ROIC', 'Margem Bruta', 'Margem Líquida']
         return {key: values for (key, values) in self.get_all_indicators().items() if key in indicators}
+    @property
+    def debt_indicators(self):
+        indicators = ['Dívida Líq/Patrim', 'Dívida Líq/EBIT',  'Passivos / Ativos']
+        # there is a pŕoblem with this indicators, because the html source get more h3 tags here, and some
+        # indicadors are not the same
+        return {key: values for (key, values) in self.get_all_indicators().items() if key in indicators}
+
+
+class Stock(Data):
+    def __repr__(self):
+        return f'BrazilianStock object {self._ticker}'
+
+
 
 if __name__ == "__main__":
     '''request = get('https://statusinvest.com.br/acoes/cvcb3').text
-
     soup = BeautifulSoup(request, 'html.parser')
     print(soup.select("div.width-auto:nth-child(2)"))'''
-    cvc = Data(ticker='cvcb3')
+    petr = Data(ticker='petr4')
+    cvc = Data('cvcb3')
     print(cvc.get_all_indicators())
-    print(cvc.profit_indicators)
-
+    print(petr.profit_indicators)
